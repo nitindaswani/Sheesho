f9e7091214e545c883a4aa455447473e152d4480
nitindaswani <nitindaswani771@gmail.com>
2026-03-30 17:57:43 +0530

    refined some things

--- sheesho/settings.py
+++ sheesho/settings.py
@@ -86,28 +86,28 @@ WSGI_APPLICATION = 'sheesho.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 # for MySQL
-import os
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sheesho',
-        'USER': 'root',  # Your MySQL username
-        'PASSWORD': '2006',  # Will use environment variable or prompt
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
-    }
-}
-
-# Backup SQLite configuration (commented out)
+# import os
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'sheesho',
+#         'USER': 'root',  # Your MySQL username
+#         'PASSWORD': '2006',  # Will use environment variable or prompt
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#         },
 #     }
 # }
+
+# Backup SQLite configuration (commented out)
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 
